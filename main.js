0741bdc1d0af149c5dbc80359298d24c62f5559d
M.Asad <asad497804@gmail.com>
2024-06-25 12:05:38 +0500

    initial Commit

---
+++ main.js
@@ -0,0 +1,11 @@
+  let feedEl=document.getElementById("jspt")
+  let msgEl = document.getElementById("comment")
+function ab(){
+  feedEl.innerText="Thanks For Your Feedback 🙂"
+}
+function bc(){
+  feedEl.innerText="Sorry! We will try to improve 😔"
+}
+function msg(){
+  comment.innerText="THANKS!"
+}
